99b59b2eb97bde9626f31879a191f0e3e4f5fbcf
ibiinc1 <99874975+ibiinc1@users.noreply.github.com>
2022-02-17 10:17:29 -0800

    Update Arbitrage.sol

--- Arbitrage.sol
+++ Arbitrage.sol
@@ -13,14 +13,15 @@ contract Arbitrage {
 using SafeMath for uint;
 
     address private owner;
-    address private constant pancakeFactory = 0xBCfCcbde45cE874adCB698cC183deBcF17952812;
-    address private constant bakeryRouter = 0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F;
+     address public constant pancakeFactory = 0xBCfCcbde45cE874adCB698cC183deBcF17952812;
+     address public constant bakeryRouter = 0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F;
     address token1 = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address token0 = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
 
   constructor(address _pancakeFactory, address _bakeryRouter) public {
     pancakeFactory = _pancakeFactory;  
     bakeryRouter = IUniswapV2Router02(_bakeryRouter);
+    uint256 _amount = 100 * 1e18; 
     
   }
 
