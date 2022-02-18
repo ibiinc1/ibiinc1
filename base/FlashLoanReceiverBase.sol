cf91303a2256ebd15264b92723c2deeba153923a
ibiinc1 <99874975+ibiinc1@users.noreply.github.com>
2022-02-18 07:54:27 -0800

    Update FlashLoanReceiverBase.sol

--- base/FlashLoanReceiverBase.sol
+++ base/FlashLoanReceiverBase.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/math/SafeMath.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/token/ERC20/SafeERC20.sol";
-import "../interfaces/IFlashLoanReceiver.sol";
-import "../interfaces/ILendingPoolAddressesProvider.sol";
+import "https://github.com/ibiinc1/ibiinc1/blob/main/interfaces/IFlashLoanReceiver.sol";
+import "https://github.com/ibiinc1/ibiinc1/blob/main/interfaces/ILendingPoolAddressesProvider.sol";
 
 contract FlashLoanReceiverBase is IFlashLoanReceiver {
     using SafeERC20 for IERC20;
