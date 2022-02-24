284c85d7ccac55a169965a689fca4c2cdb05af40
ibiinc1 <99874975+ibiinc1@users.noreply.github.com>
2022-02-24 04:35:36 -0800

    Update Arbi.sol

--- Arbi.sol
+++ Arbi.sol
@@ -1,12 +1,9 @@
-pragma solidity ^0.6.6;
+pragma solidity ^0.5.0;
 
-
-contract Arbitrage {
   address public constant pancakeFactory = 0xBCfCcbde45cE874adCB698cC183deBcF17952812;
      address public constant bakeryRouter = 0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F;
     address token1 = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address token0 = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
-  }
-
+ 
   
  
