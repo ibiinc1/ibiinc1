5799997ca5fe730a302a2ed2017f0ae15b7fa36c
ibiinc1 <99874975+ibiinc1@users.noreply.github.com>
2022-02-24 04:34:24 -0800

    Update Arbi.sol

--- Arbi.sol
+++ Arbi.sol
@@ -1,16 +1,12 @@
 pragma solidity ^0.6.6;
 
-contract Arbi {
-using SafeMath for uint;
 
-    address private owner;
-     address public constant pancakeFactory = 0xBCfCcbde45cE874adCB698cC183deBcF17952812;
+contract Arbitrage {
+  address public constant pancakeFactory = 0xBCfCcbde45cE874adCB698cC183deBcF17952812;
      address public constant bakeryRouter = 0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F;
     address token1 = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address token0 = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
-   function flashloanBnb(uint256 _amount) public  {
-          bytes memory data = "";
-        uint256 _amount = 10;
   }
 
+  
  
