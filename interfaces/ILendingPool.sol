236196dbcfd87ad37da945dc3243573d52b78c15
ibiinc1 <99874975+ibiinc1@users.noreply.github.com>
2022-03-04 09:48:38 -0800

    Update ILendingPool.sol

--- interfaces/ILendingPool.sol
+++ interfaces/ILendingPool.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0;
 
 interface ILendingPool {
     function flashLoan(address _receiver, address _reserve, uint256 _amount, bytes calldata _params) external;
